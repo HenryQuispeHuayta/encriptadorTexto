1691c9b1acc9616b634a77e61f0580af71d8d697
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2022-12-23 22:21:28 -0400

    replace en js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,48 @@
+// let textoEncriptado = document.getElementById("textoEncriptar");
+// let textoDesencriptado = document.getElementById("textoDesencriptar");
 
-function encriptar(){
-  let texto = document.getElementById('textoEncriptar').value;
-  let textoEncriptado = document.getElementById('textoDesencriptar').innerHTML;
-  let botonEncriptar = document.getElementById('botonEncriptar');
+let textoEncriptado = document.querySelector('#textoEncriptar');
+let textoDesencriptado = document.querySelector('#textoDesencriptar');
 
-  textoEncriptado = texto;
+// let botonEncriptar = document.getElementById("botonEncriptar");
+// let botonDesencriptar = document.getElementById("botonDesencriptar");
+
+let botonEncriptar = document.querySelector("#botonEncriptar");
+let botonDesencriptar = document.querySelector("#botonDesencriptar");
+
+let historial = document.getElementById("historial");
+
+function encriptar() {
+  let texto = textoEncriptado.value;
+  texto = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+  textoDesencriptado.innerText = texto;
+  // historial.innerText = salida2;
 }
 
-botonEncriptar.onclick = encriptar;
+function desenciptar() {
+  let textoDes = textoDesencriptado.value;
+  textoDes = textoDes
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  textoEncriptado.innerText = textoDes;
+}
+
+botonEncriptar.addEventListener("click", encriptar);
+botonDesencriptar.addEventListener("click", desenciptar);
+
+// botonEncriptar.onclick = encriptar;
+
+// let boton = document.getElementById('botonEncriptar');
+// function alerta() {
+//   alert('hu');
+// }
+// boton.addEventListener('click', alerta);
+// boton.add
